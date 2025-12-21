a7bd64a49a0846e10fdc817ea87bc18bd86a164d
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-12-22 01:16:26 +0300

    İkon Değiştirildi Ve Birkaç Ayar Yapıldı.

--- BerberAsistani/App.xaml.cs
+++ BerberAsistani/App.xaml.cs
@@ -7,10 +7,14 @@ namespace BerberAsistani
         public App()
         {
             InitializeComponent();
+
+            // Temayı güvenli bir şekilde zorla
+            UserAppTheme = AppTheme.Light;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
+            // MainPage atamasını burada yapmak en sağlıklı yoldur
             return new Window(new AppShell());
         }
     }
